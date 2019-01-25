ac31f6d2939b4309e7a9b28c96e2637d38138194
Vikram Jakhar <vikram.jakhar@tothenew.com>
2019-01-25 17:28:38 +0530

    updated block

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,11 +1,11 @@
 def info(message) {
-    echo "\033[1;33m[Info]    \033[0m ${message}"
+    echo "\033[1;33m[Info] ${message} \033[0m"
 }
 
 def success(message) {
-    echo "\033[1;32m[Success] \033[0m ${message}"
+    echo "\033[1;32m[Success] ${message} \033[0m"
 }
 
 def error(message) {
-    echo "\033[1;31m[Error]   \033[0m ${message}"
+    echo "\033[1;31m[Error] ${message} \033[0m"
 }
